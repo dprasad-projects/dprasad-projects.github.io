c7e57c1d92686fa5e1bad4fc9240d308708bdf8a
Durga Prasad <74120731+dprasad-projects@users.noreply.github.com>
2022-01-19 15:50:56 -0600

    Update agentwidgetOMDB.js

--- js/agentwidgetOMDB.js
+++ js/agentwidgetOMDB.js
@@ -1,47 +1,48 @@
+var path = data.key;
+pathToData = "chatTranscript.lines";
 var updateCallback = function(data) {
-	console.log("***updateCallback****"); // Do something with the returning data//
-	console.log(JSON.stringify(data)); //
-
-	var path = data.key;
-	var value = data.newValue;
-	var index = value.length - 1;
-	var movieObj = value[index];
-
-	console.log("***index****", index);
-	console.log("***index****", JSON.stringify(movieObj));
-
-	if (movieObj.source === "visitor") 
-	{	
-		console.log("Input Text", movieObj.text);
-		fetch("https://www.omdbapi.com?t=" + movieObj.text + "&apikey=adc5c705")
-		.then(response => response.json())
-		.then(res => {
-			console.log("Result array",res)
-			
-			if (res.Response === "True") 
-			{
-				document.getElementById("Title").innerHTML = res.Title;
-				document.getElementById("Year").innerHTML = res.Year;
-				document.getElementById("Rated").innerHTML = res.Rated;	
-				document.getElementById("Actors").innerHTML = res.Actors;
-				document.getElementById("Genre").innerHTML = res.Genre;
-				document.getElementById("Plot").innerHTML = res.Plot;
-				document.getElementById("Released").innerHTML = res.Released;
-				document.getElementById("Runtime").innerHTML = res.Runtime;
-				document.getElementById("Director").innerHTML = res.Director;
-				document.getElementById("Awards").innerHTML = res.Awards;
-				document.getElementById("imdbRating").innerHTML = res.imdbRating;
-			} else {				
-				appDiv.innerHTML = res.Error;
-			}
-		})
-		.catch(err => {
-			console.log(err);
-		});
-
-	}
-};
-
-lpTag.agentSDK.init({
-    VisitorUpdateCallback: updateCallback
+console.log("***updateCallback****"); // Do something with the returning data//
+console.log(JSON.stringify(data)); //
+var path = data.key;
+var value = data.newValue;
+var index = value.length - 1;
+var movieObj = value[index];
+console.log("***index****", index);
+console.log("***index****", JSON.stringify(movieObj));
+if (movieObj.source === "visitor") {
+fetch("https://www.omdbapi.com?t=" + movieObj.text + "&apikey=28f086dd")
+.then(response => response.json())
+.then(res => {
+// console.log(res)
+if (res.Response === "True") {
+document.getElementById("Title").innerHTML = res.Title;
+document.getElementById("Year").innerHTML = res.Year;
+document.getElementById("Rated").innerHTML = res.Rated;
+document.getElementById("Actors").innerHTML = res.Actors;
+document.getElementById("Genre").innerHTML = res.Genre;
+document.getElementById("Plot").innerHTML = res.Plot;
+document.getElementById("Released").innerHTML = res.Released;
+document.getElementById("Runtime").innerHTML = res.Runtime;
+document.getElementById("Director").innerHTML = res.Director;
+document.getElementById("Awards").innerHTML = res.Awards;
+document.getElementById("imdbRating").innerHTML = res.imdbRating;
+} else {
+appDiv.innerHTML = res.Error;
+document.getElementById("errMessage").innerHTML = res.Error;
+}
+})
+.catch(err => {
+console.log(err);
 });
+}
+};
+var notifyWhenDone =function(err) {
+if (err){
+// Do something with the error
+console.log("error",err);
+}
+// called when the bind is completed successfully,
+// or when the action terminated with an error.
+};
+lpTag.agentSDK.init({});
+lpTag.agentSDK.bind(pathToData,updateCallback, notifyWhenDone);
